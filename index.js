afa5193e121de56127dc4264b007cc632156ad39
ardizzonepietro <ardizzone.pietro@studenti.salesianisesto.it>
2021-12-14 14:21:38 +0100

    renspond client from get home

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const { response } = require("express");
 var express = require("express");
 var apiServer= express();
 var port = 300;
@@ -6,3 +7,7 @@ apiServer.listen(port, host, ()=>{
     console.log("server is running: http://%s:%d", host, port); //set server port and link
 })
 
+apiServer.get("/", (request, response)=>{
+    console.log("enter get")
+    response.send("<h1>ciao client!</h1>")
+})
